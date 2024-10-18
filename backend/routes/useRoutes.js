43a584aa20811c7af05f2758c8b136723c63d25f
IreVictoria <irenevictoriaeg17@gmail.com>
2024-10-18 00:19:08 -0300

    arreglo codigo 3

--- backend/routes/useRoutes.js
+++ backend/routes/useRoutes.js
@@ -5,12 +5,12 @@ const authRouter = express.Router();
 //IMPORTAR MIDDLEWARE.
 const auth = require(`../middleware/authorization`);
 //IMPORTAR FUNCIONES DEL CONTROLADOR. 
-const { registerUser, loginUser, verifyToken, getAllUsers, updateUser  } = require(`../controllers/userController`);
+const { registerUser, loginUser, verifyToken, getUserById, updateUser  } = require(`../controllers/userController`);
 
 authRouter.post(`/register`, registerUser);
 authRouter.post(`/login`, loginUser);
 authRouter.get(`/verify-token`, auth, verifyToken);
-authRouter.get(`/`, getAllUsers); 
+authRouter.get(`/:id`, getUserById); 
 authRouter.get(`/:id`, updateUser); 
 
 module.exports = authRouter; 
